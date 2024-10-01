8e6ee746a0279747d0daafa59fdfc0d4e65617ac
Jiazheng Fan <jzfan.mail@gmail.com>
2024-10-01 21:48:49 +0800

    Add test comment to SU2_CFD.cpp

--- SU2_CFD/src/SU2_CFD.cpp
+++ SU2_CFD/src/SU2_CFD.cpp
@@ -24,7 +24,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
  */
-
+//test comment
 #include "../include/SU2_CFD.hpp"
 
 /* LIBXSMM include files, if supported. */
